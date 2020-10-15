182de89113cfc0fdd8d0e2f13e084a8f7d4d6984
leehani <rorean@naver.com>
2020-10-16 00:12:32 +0900

    Mock 객체Stubbing 연습 문제

--- src/test/java/com/example/junit5/study/StudyServiceTest.java
+++ src/test/java/com/example/junit5/study/StudyServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.junit5.study;
 
 import com.example.junit5.domain.Member;
+import com.example.junit5.domain.Study;
 import com.example.junit5.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -64,4 +65,27 @@ class StudyServiceTest {
 
         assertEquals(Optional.empty(), memberService.findById(1L));
     }
-}
+
+    @Test
+    void stubbing_연습() {
+        StudyService studyService  = new StudyService(memberService, studyRepository);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("e@email.com");
+
+        Study study = new Study(10, "test");
+        study.setOwner(member);
+
+        // TODO memberService 객체에 findById 메소드를 1L 값으로 호출하면 Optional.of(member) 객체를 리턴하도록 Stubbing
+        // TODO studyRepository 객체에 save 메소드를 study 객체로 호출하면 study 객체 그대로 리턴하도록 Stubbing
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        when(studyRepository.save(study)).thenReturn(study);
+
+        studyService.createNewStudy(1L, study);
+
+        assertNotNull(study.getOwner());
+        assertEquals(member, study.getOwner());
+
+    }
+ }
