8cebdd79f3937eb4582a379a7e5426134a78cc51
leehani <rorean@naver.com>
2021-02-01 17:22:20 +0900

    Testcontainer 테스트 코드 클래스 분리

--- src/test/java/com/example/junit5/study/TestcontainersTest.java
+++ src/test/java/com/example/junit5/study/TestcontainersTest.java
@@ -3,24 +3,55 @@ package com.example.junit5.study;
 import com.example.junit5.domain.Member;
 import com.example.junit5.domain.Study;
 import com.example.junit5.member.MemberService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
-class StudyServiceTest {
+@ActiveProfiles("test")
+@Testcontainers
+class TestcontainersTest {
 
     @Mock
     MemberService memberService;
 
-    @Mock StudyRepository studyRepository;
+    @Autowired StudyRepository studyRepository;
+
+    //여러 테스트에서 공유하기 위해 static 선언
+    @Container
+    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+            .withDatabaseName("studytest");
+
+    @BeforeEach
+    void beforeEach() {
+        studyRepository.deleteAll();
+    }
+
+//    @BeforeAll
+//    static void beforeAll() {
+//        postgreSQLContainer.start();
+//        System.out.println(postgreSQLContainer.getJdbcUrl());
+//    }
+//
+//    @AfterAll
+//    static void afterAll() {
+//        postgreSQLContainer.stop();
+//    }
 
     @Test
     void createStudy() {
@@ -98,8 +129,6 @@ class StudyServiceTest {
         StudyService studyService = new StudyService(memberService, studyRepository);
         Study study = new Study(10, "더 자바, 테스트");
 
-        given(studyRepository.save(study)).willReturn(study);
-
         // When
         Study result = studyService.openStudy(study);
 
@@ -110,4 +139,6 @@ class StudyServiceTest {
         // memberService의 notify(study)가 호출 됐는지 확인.
         then(memberService).should(times(1)).notify(study);
     }
+
+
 }
